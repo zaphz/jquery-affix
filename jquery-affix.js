46309ee1eefb3c6ec7de3bf4acbadf1f43b583e4
Babbitt huang <bhuang@bbtechgroup.com>
2014-02-25 17:24:09 +0800

    [web] update

---
+++ jquery-affix.js
@@ -0,0 +1,29 @@
+/* Created by frank on 2/25/14. */
+(function ($) {
+    $.fn.affix = function (_options) {
+        if (this.data('affixInited')) {
+            this.data('affixOptions').top = _options.top || 0
+            checkScrollTop.call(this)
+            return
+        }
+        this.data('affixInited', true)
+
+        this.data('affixOptions', {
+            top: _options.top || 0
+        })
+
+        $(window).on('scroll', checkScrollTop.bind(this))
+
+
+        function checkScrollTop() {
+            if ($(window).scrollTop() < this.data('affixOptions').top) {
+                this.removeClass('affix-on')
+            } else {
+                this.addClass('affix-on')
+            }
+        }
+
+        checkScrollTop.call(this)
+
+    }
+})(jQuery)
